68a88991e623d071107ad0f85f050b7ee9a6a88a
AbogeJr <junioraboge@gmail.com>
2023-06-06 14:15:06 +0300

    initialize module

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Autolamps Custom Module",
+    "version": "1.0",
+    "depends": [
+        "base",
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": True,
+    "license": "LGPL-3",
+}
