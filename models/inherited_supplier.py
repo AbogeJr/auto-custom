9cedfe77c99ebf7daba6be5693a3085ac3de3d3f
Zainab <zainabchini@gmail.com>
2023-06-08 10:56:01 +0300

    [FIX] change supplier_type

--- models/inherited_supplier.py
+++ models/inherited_supplier.py
@@ -15,5 +15,20 @@ class StockWarehouseOrderpoint(models.Model):
     _inherit = "stock.warehouse.orderpoint"
 
     supplier_type = fields.Selection(
-        related="supplier_id.supplier_type", string="Locality"
+        [("local", "Local"), ("international", "International")],
+        string="Locality",
+        default="local"
     )
+
+    _sql_constraints = [
+        (
+            "supplier_locality_check",
+            "unique (product_id, supplier_type)",
+            "You can not have more than one supplier from the same locality!",
+        ),
+        (
+            "product_location_check",
+            "CHECK(1=1)",
+            "A replenishment rule already exists.",
+        ),
+    ]
