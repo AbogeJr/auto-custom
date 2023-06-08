a38478c6b923d4bd7e69a5a433686b4509f096be
Zainab <zainabchini@gmail.com>
2023-06-08 10:56:01 +0300

    [FIX] change supplier_type

--- __manifest__.py
+++ __manifest__.py
@@ -3,8 +3,8 @@
     "version": "1.0",
     "depends": ["base", "sale_management", "stock", "product"],
     "data": [
-        "views/inherited_supplier_views.xml",
         "views/product_supplierinfo_views.xml",
+        "views/inherited_supplier_views.xml",
     ],
     "installable": True,
     "application": True,
