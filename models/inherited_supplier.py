a71c11718affdbe238aa425e694d2cb4315e99e9
AbogeJr <junioraboge@gmail.com>
2023-06-12 14:10:55 +0300

    update: extend reordering rules logic

--- models/inherited_supplier.py
+++ models/inherited_supplier.py
@@ -45,68 +45,23 @@ class InheritStockWarehouseOrderpoint(models.Model):
     def _procure_orderpoint_confirm(
         self, use_new_cursor=False, company_id=None, raise_user_error=True
     ):
-        international_orderpoints = self.filtered(
-            lambda op: op.supplier_type == "international"
-        )
-        local_orderpoints = self.filtered(lambda op: op.supplier_type == "local")
+        orderpoints = self.filtered(lambda op: op.qty_available < op.product_min_qty)
+        for orderpoint in orderpoints:
+            vendor = orderpoint.supplier_id
 
-        if international_orderpoints:
-            for orderpoint in international_orderpoints:
-                if orderpoint.qty_available < orderpoint.product_min_qty:
-                    # Trigger replenishment from international vendor
-                    vendor = (
-                        orderpoint.supplier_id
-                    )  # Assuming you have a field named 'international_vendor_id' on the orderpoint model
-                    if vendor:
-                        # Create procurement for the international vendor
-                        procurement_vals = {
-                            "product_id": orderpoint.product_id.id,
-                            "product_qty": orderpoint.qty_to_order,
-                            "product_uom": orderpoint.product_uom.id,
-                            "partner_id": vendor.id,
-                            "origin": orderpoint.name,
-                            "company_id": orderpoint.company_id.id,
-                        }
-                        self.env["procurement.group"].run(
-                            [
-                                self.env["procurement.group"].Procurement(
-                                    **procurement_vals
-                                )
-                            ]
-                        )
-
-        if local_orderpoints:
-            for orderpoint in local_orderpoints:
-                if orderpoint.qty_available == 0:
-                    # Cancel the previous replenishment (if any)
-                    procurements = self.env["procurement.group"].search(
-                        [
-                            ("orderpoint_id", "=", orderpoint.id),
-                            ("state", "not in", ["done", "cancel"]),
-                        ]
-                    )
-                    if procurements:
-                        procurements.sudo().cancel()
-
-                    # Trigger replenishment from local vendor
-                    vendor = orderpoint.supplier_id
-                    if vendor:
-                        # Create procurement for the local vendor
-                        procurement_vals = {
-                            "product_id": orderpoint.product_id.id,
-                            "product_qty": orderpoint.qty_to_order,
-                            "product_uom": orderpoint.product_uom.id,
-                            "partner_id": vendor.id,
-                            "origin": orderpoint.name,
-                            "company_id": orderpoint.company_id.id,
-                        }
-                        self.env["procurement.group"].run(
-                            [
-                                self.env["procurement.group"].Procurement(
-                                    **procurement_vals
-                                )
-                            ]
-                        )
+            if vendor:
+                # Create procurement for the vendor
+                procurement_vals = {
+                    "product_id": orderpoint.product_id.id,
+                    "product_qty": orderpoint.qty_to_order,
+                    "product_uom": orderpoint.product_uom.id,
+                    "partner_id": vendor.id,
+                    "origin": orderpoint.name,
+                    "company_id": orderpoint.company_id.id,
+                }
+                self.env["procurement.group"].run(
+                    [self.env["procurement.group"].Procurement(**procurement_vals)]
+                )
 
         return super(InheritStockWarehouseOrderpoint, self)._procure_orderpoint_confirm(
             use_new_cursor=use_new_cursor,
